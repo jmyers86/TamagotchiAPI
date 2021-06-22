c97781cf8d57d3f936c899b3112c30b75f389ac6
Joe Myers <joecaricature@gmail.com>
2021-06-22 19:29:21 -0400

    Initial Migration

---
+++ Models/Scolding.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TamagotchiAPI.Models
+{
+    public class Scolding
+    {
+        public int Id { get; set; }
+
+        public DateTime When { get; set; }
+
+        public int PetId { get; set; }
+
+        public Pet Pet { get; set; }
+    }
+}
